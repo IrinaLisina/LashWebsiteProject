df89d446d00e283a46830ca5f428c422fccf6077
Irina <nxij@hotmail.co.uk>
2022-02-02 18:49:57 +0000

    uploading website project files for react.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Article, Brand, Cta, Navbar, Feature } from './components';
+
+const App = () => {
+  return (
+        <div>
+            <h1>Lash & Dash</h1>
+        </div>
+    );
+};
+
+export default App;
